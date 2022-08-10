345ba41a30c4da83244bb3cbe57ae35f4f72e2be
musawakiliML <musawakiliml@hotmail.com>
2022-08-10 12:27:15 +0100

    Added metadat extraction

--- Fetch_Meta_Data_App/utils_functions/extract_meta_data.py
+++ Fetch_Meta_Data_App/utils_functions/extract_meta_data.py
@@ -1,8 +1,13 @@
 # Import Libraries
 import pikepdf
-
+from hachoir.parser import createParser
+from hachoir.metadata import extractMetadata
+from exif import Image
+from PIL import Image as im
 
 # Extract Pdf metadata
+
+
 def get_pdf_metadata(filename):
     '''This function extract the metadata of pdf files and returns it as a dictionary object.'''
     pdf = pikepdf.Pdf.open(filename)
@@ -11,3 +16,33 @@ def get_pdf_metadata(filename):
     for key, value in docinfo.items():
         docinfo_dict[key] = str(value)
     return docinfo_dict
+
+
+# Extract Media Metadata
+def get_media_metadata(filename):
+    '''This function returns the metadata of any supported file as a list
+
+        params: filename - the name of the file passed
+    '''
+    file_parser = createParser(filename)
+    if not file_parser:
+        return "Unable to parse a file!!"
+    with file_parser:
+        try:
+            file_metadata = extractMetadata(file_parser)
+            return file_metadata.exportPlaintext()
+        except Exception as err:
+            file_metadata = None
+            return file_metadata  # "Metadata Extraction Error!!"
+
+
+test = get_media_metadata(
+    'Fetch_Meta_Data_App/static/upload/image1.jpeg')
+
+
+img_path = f'Fetch_Meta_Data_App/static/upload/80.png'
+
+with open(img_path, 'rb') as img_file:
+    img = Image(img_file)
+
+print(img.has_exif)
